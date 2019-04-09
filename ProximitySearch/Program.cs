379fdd4206c7544dbf50147364da9f3fb552582a
Nallabolu <212561870@logon.ds.ge.com>
2019-04-09 07:45:00 -0500

    console way

--- ProximitySearch/Program.cs
+++ ProximitySearch/Program.cs
@@ -14,18 +14,24 @@ namespace ProximitySearch
         {
             try
             {
-                ValidationService.ValidateNumberOfArgumentsProvided(args);
+                Console.WriteLine("Please enter input as KeyWord1 KeyWord2 Range and input file path");
+                var input = Console.ReadLine();
+                var inputArray = input.Split(' ');
+                ValidationService.ValidateNumberOfArgumentsProvided(inputArray);
                 var processCommandService = new ProcessCommandsService();
-                var keyWordsOccurenceWithinRange = processCommandService.Process(args[0], args[1], args[2], args[3]);
+                var keyWordsOccurenceWithinRange = processCommandService.Process(inputArray[0], inputArray[1], inputArray[2], inputArray[3]);
                 Console.WriteLine(keyWordsOccurenceWithinRange);
+                Main(new string[0]);
             }
             catch (InputValidationException ex)
             {
                 Console.WriteLine(ex.Message);
+                Main(new string[0]);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 Console.WriteLine("unknown error occured");
+                Main(new string[0]);
             }
         }
     }
